7106653743247c29720d8828d0c8d0df933be4f5
lemarc <lemarc.dev@gmail.com>
2024-09-03 10:48:27 +0200

    prettify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { Client } from "@opensearch-project/opensearch";
+import { Client } from '@opensearch-project/opensearch';
 
 export const connect = async (ctx) => {
   const { logger } = ctx;
-  logger.info("Attempting to connect to OpenSearch");
+  logger.info('Attempting to connect to OpenSearch');
 
   try {
     const client = new Client({
       node: ctx.env.opensearch.url,
-      // Add other configuration options as needed, such as:
+
       // auth: {
       //   username: ctx.env.opensearch.username,
       //   password: ctx.env.opensearch.password,
@@ -19,13 +19,13 @@ export const connect = async (ctx) => {
 
     // Test the connection
     await client.ping();
-    logger.info("Successfully connected to OpenSearch");
+    logger.info('Successfully connected to OpenSearch');
 
     ctx.dbs.opensearch = {
       client,
     };
   } catch (error) {
-    logger.error(`Failed to connect to OpenSearch: ${error.message}`, "error");
+    logger.error(`Failed to connect to OpenSearch: ${error.message}`, 'error');
     throw error;
   }
 };
@@ -33,18 +33,15 @@ export const connect = async (ctx) => {
 export const disconnect = async (ctx) => {
   const { logger } = ctx;
   if (ctx.dbs?.opensearch?.client) {
-    logger.info("Disconnecting from OpenSearch");
+    logger.info('Disconnecting from OpenSearch');
     try {
       await ctx.dbs.opensearch.client.close();
-      logger.info("Successfully disconnected from OpenSearch");
+      logger.info('Successfully disconnected from OpenSearch');
     } catch (error) {
-      logger.error(
-        `Error during OpenSearch disconnection: ${error.message}`,
-        "error"
-      );
+      logger.error(`Error during OpenSearch disconnection: ${error.message}`, 'error');
       throw error;
     }
   } else {
-    logger.warn("No active OpenSearch connection to disconnect", "warn");
+    logger.warn('No active OpenSearch connection to disconnect', 'warn');
   }
 };
